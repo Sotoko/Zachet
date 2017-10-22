0aef7296dc6bc807442bb2ffe90dde3dcd5c045c
Natalia_02.90 <Natalia_02.90@mail.ru>
2017-10-22 21:25:21 +0500

    изменения

---
+++ коровы форма/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Коровы;
+
+namespace коровы_форма
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            int userDigit = int.Parse(textBox1.Text);
+            label2.Text = Коровы.Program.CowCounter(userDigit);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
